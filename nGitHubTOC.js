6c01d9e25e5e385f49419127cfa0d21e8fb31f66
Ruzgfpegk <Ruzgfpegk@zero-sum.fr>
2021-03-24 03:41:38 +0900

    Allows to read other code blocks than shell ones.

--- nGitHubTOC.js
+++ nGitHubTOC.js
@@ -11,7 +11,7 @@ function tocIt(inputMD, minHeading, maxHeading, ignoreLinex)
     {
         var inputMDLine = inputMDLines[i];
 
-        var codeTag = /^(`{3})(shell)?$/.exec(inputMDLine);
+        var codeTag = /^(`{3})(.*)?$/.exec(inputMDLine);
         if(codeTag)
         {
             codeTagEndExpected = !codeTagEndExpected;
